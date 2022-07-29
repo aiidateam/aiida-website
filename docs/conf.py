b1c0fe1d8007ce2869cd6c804eaddfb52db6d01c
Chris Sewell <chrisj_sewell@hotmail.com>
2022-07-29 12:17:11 +0200

    👌 IMPROVE: Fix re-direct warnings on re-builds

--- docs/conf.py
+++ docs/conf.py
@@ -157,18 +157,44 @@ def build_redirects(app: Sphinx, exception: Optional[Exception]) -> None:
     """Build and write redirects."""
     if app.builder.format != "html":
         return
-    logger.info("Writing redirects...")
+    logger.info("Writing legacy redirects...")
+
+    # read records from previous build, if any
+    redirect_json_file = Path(app.outdir) / "_legacy_redirected.json"
+    if redirect_json_file.exists():
+        redirect_record = json.loads(redirect_json_file.read_text("utf8"))
+    else:
+        redirect_record: Dict[str, str] = {}
+
     build_redirect_base = Path(app.outdir)
     redirects: Dict[str, str] = json.loads(
         Path(app.srcdir).joinpath("legacy_redirect.json").read_text()
     )
     for redirect_from, redirect_to in redirects.items():
+
+        # create paths
         if redirect_from.endswith("/"):
             redirect_from = redirect_from + "index.html"
         build_redirect_from = build_redirect_base / redirect_from
+        build_redirect_to = build_redirect_base / (redirect_to + ".html")
+
+        # deal with redirect written from previous builds
+        if (
+            build_redirect_from.exists()
+            and build_redirect_from.as_posix() in redirect_record
+        ):
+            # if it is still pointing to the same source, continue
+            if (
+                redirect_record[build_redirect_from.as_posix()]
+                == build_redirect_to.as_posix()
+            ):
+                continue
+            # otherwise remove and rewrite
+            build_redirect_from.unlink()
+
+        # create redirect file
         if build_redirect_from.exists():
             logger.warning(f"redirect-from already exists: {build_redirect_from}")
-        build_redirect_to = build_redirect_base / (redirect_to + ".html")
         if not build_redirect_to.exists():
             logger.warning(f"redirect-to does not exist: {build_redirect_to}")
         build_redirect_from.parent.mkdir(parents=True, exist_ok=True)
@@ -177,6 +203,10 @@ def build_redirects(app: Sphinx, exception: Optional[Exception]) -> None:
                 rel_url=relpath(build_redirect_to, build_redirect_from.parent)
             )
         )
+        # record for re-builds
+        redirect_record[build_redirect_from.as_posix()] = build_redirect_to.as_posix()
+
+    redirect_json_file.write_text(json.dumps(redirect_record), encoding="utf8")
 
 
 # ablog custom code
