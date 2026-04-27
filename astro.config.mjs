eee5f109729572458f47e713525c12bd769ab23b
Ali Khosravi <khsrali@gmail.com>
2026-04-27 21:09:32 +0200

    feat(seo): add canonical, og tags, sitemap, and cloudflare redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://aiida.net",
   base: "/",
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
